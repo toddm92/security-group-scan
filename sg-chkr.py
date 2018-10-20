79eb74340ee17de67040545e56ddd85ad7db2b84
Todd Murchison <toddm92@gmail.com>
2018-10-19 17:07:17 -0700

    initial rewrite

--- sg-chkr.py
+++ sg-chkr.py
@@ -1,97 +1,167 @@
-#!/usr/bin/env python
-#
-# Python Version: 2.7
-#
-# Scan for "open" security groups 
+"""
 
-# Must be the first line
-from __future__ import print_function
+Scan AWS VPC security groups for rule violations.
 
-import boto.vpc
-import boto.ec2
-import sys
+Python Version: 3.7.0
+Boto3 Version: 1.7.50
 
-# ** Modify these variables as needed **
-REGIONS = ( 'us-east-1',  'eu-west-1',  'ap-northeast-1',
-            'us-west-1', 'us-west-2', 'ap-southeast-1',
-            'ap-southeast-2', 'sa-east-1', 'eu-central-1' )
-# **
+"""
 
-# Make our text pretty
-class bcolors:
-    HEADER = '\033[95m'
-    BLUE = '\033[94m'
-    GREEN = '\033[92m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
-    ENDC = '\033[0m'
+import boto3
+from botocore.exceptions import ClientError
 
-def usage():
-    """Usage Statement"""
 
-    print("""
+def get_vpcs(ec2):
+  """
+  Return all VPCs
+  """
 
-  Security group checker..
-    """)
+  vpcs = []
 
-    print("\t* use: --profile <profile_name>")
-    print("\t" + bcolors.GREEN + "profile_name " + bcolors.ENDC + "from your ~/.boto (~/.aws/config)\n\n")
+  try:
+    aws_vpcs = ec2.describe_vpcs()['Vpcs']
+  except ClientError as e:
+    print(e.response['Error']['Message'])
 
-    exit()
+  else:
+    for vpc in aws_vpcs:
+      vpcs.append(vpc['VpcId'])
 
-# Check for arguments
-args = len(sys.argv)
-if args != 3:
-    usage()
+  return vpcs
 
-if '--profile' in (str(sys.argv[1]).lower()):
-    profile = str(sys.argv[2]).lower()
-else:
-    usage()
 
-reg_total = len(REGIONS)
-reg_no = 0
+def get_regions(ec2):
+  """
+  Return all AWS regions
+  """
 
-# Test in each region (main loop)
-while reg_total > 0:
+  regions = []
 
-    # Do the work
-    sg_no = 0
-    reg_name = REGIONS[reg_no]
-    myregion = boto.ec2.get_region(region_name=reg_name)
-    try:
-        conn = boto.vpc.VPCConnection(profile_name=profile, region=myregion)
-    except Exception,e:
-        print("\nCheck your profile_name in ~/.boto and try again.")
-        print(e)
-        usage()
+  try:
+    aws_regions = ec2.describe_regions()['Regions']
+  except ClientError as e:
+    print(e.response['Error']['Message'])
 
-    print("\nRegion:", reg_name)
+  else:
+    for region in aws_regions:
+      regions.append(region['RegionName'])
 
-    # Get all security groups
-    all_sgs = conn.get_all_security_groups()
-    sg_total = len(all_sgs)
-    print("Number of SGs:", sg_total, "\n")
+  return regions
 
-    # Scan the rules in each security group 
-    # Look for 0.0.0.0/0 as the source - ports 80 and 443 probably ok, but print them anyway
-    while sg_no < sg_total:
 
-        sg = all_sgs[sg_no]
-        for rule in sg.rules:
-            if str(rule.from_port) == "80" or str(rule.from_port) == "443":
-                textc = bcolors.WARNING
+def main(profile):
+  """
+  Do the work..
+  """
+
+  # Violation lists
+
+  port_list = [ 'tcp-22',
+                'tcp-23',
+                'tcp-3389'
+              ]
+
+  cidr_list = [
+                '0.0.0.0/0',
+                '::/0'
+              ]
+
+  # AWS Credentials
+  # https://boto3.amazonaws.com/v1/documentation/api/latest/guide/configuration.html
+
+  session = boto3.Session(profile_name=profile)
+  ec2 = session.client('ec2', region_name='us-east-1')
+
+  regions = get_regions(ec2)
+
+  for region in regions:
+    print('\nRegion: {}'.format(region))
+
+    ec2  = session.client('ec2', region_name=region)
+    vpcs = get_vpcs(ec2)
+
+    if len(vpcs) <= 0:
+      print('No security groups to check.')
+      continue
+
+    # Checks..
+
+    for vpc_id in vpcs:
+
+      args = {
+        'Filters' : [
+          {
+            'Name' : 'vpc-id',
+            'Values' : [ vpc_id ]
+          }
+        ]
+      }
+
+      try:
+        groups = ec2.describe_security_groups(**args)['SecurityGroups']
+      except ClientError as e:
+        print(e.response['Error']['Message'])
+        return
+
+      print('Checking {:d} security group(s) in VPC {}..\n'.format(len(groups), vpc_id))
+
+      for group in groups:
+        group_id   = group['GroupId']
+        group_name = group['GroupName']
+
+        for ip_perm in group['IpPermissions']:
+          ip_protocol = ip_perm['IpProtocol']
+
+          if ip_protocol == '-1':
+            from_port   = 'all'
+            to_port     = 'all'
+            ip_protocol = 'all'
+
+          else:
+            from_port   = ip_perm['FromPort']
+            to_port     = ip_perm['ToPort']
+
+          ip_ranges   = []
+
+          # IPv4 source IP ranges
+
+          if 'IpRanges' in ip_perm:
+            for ip_range in ip_perm['IpRanges']:
+              ip_ranges.append(ip_range['CidrIp'])
+
+          # IPv6 source IP ranges
+
+          if 'Ipv6Ranges' in ip_perm:
+            for ip_range in ip_perm['Ipv6Ranges']:
+              ip_ranges.append(ip_range['CidrIpv6'])
+
+          for cidr_ip in ip_ranges:
+            violations = []
+
+            if ip_protocol == 'all' and cidr_ip in cidr_list:
+              violations.append(ip_perm)
+
             else:
-                textc = bcolors.FAIL
-            for grant in rule.grants:
-                if str(grant) in ("0.0.0.0/0"):
-                    print(textc + "WARNING: Open security group >>>", sg.name, "(", sg.id, ")")
-                    textc = bcolors.ENDC
-                    print(textc + "Proto:", rule.ip_protocol, "\tPorts:", rule.from_port, "\t", rule.to_port, "\tSource:", rule.grants, "\n")
+              for port in port_list:
+                first, last = port.split('-', 1)
 
-        sg_no += 1
+                if from_port <= int(last) <= to_port and ip_protocol.lower() == first and cidr_ip in cidr_list:
+                  violations.append(ip_perm)
 
-    reg_no += 1
-    reg_total -= 1
-#
-# (end main loop)
+            if len(violations) > 0:
+              print('\033[1m'+'Violation:'+'\033[0m'+' security group >>> {} ( {} )'.format(group_name, group_id))
+              print('proto: {}\t from port: {}\t to port: {}\t source: {}\n'.format(ip_protocol, from_port, to_port, cidr_ip))
+
+          # end ip_ranges loop
+        # end ip_perms loop
+      # end groups loop
+    # end vpcs loop
+  # end regions loop
+
+  return
+
+
+if __name__ == "__main__":
+
+  main(profile = 'charlie')
+
